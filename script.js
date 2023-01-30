7ff53bcbb6d382513c0b718c0740d1d766e5daae
Raghav2222-ai <119906489+Raghav2222-ai@users.noreply.github.com>
2023-01-30 15:03:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(my,1000)
+function  my(){
+    let d = new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hrota=30*htime+mtime/2
+    mrota=6*mtime;
+    srota=6*stime;
+    hour.style.transform = `rotate(${hrota}deg`;
+    minute.style.transform = `rotate(${mrota}deg)`;
+    second.style.transform = `rotate(${srota}deg)`;  
+}
